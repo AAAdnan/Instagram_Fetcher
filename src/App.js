40038a26777d632582ae9d99ae4d695c6c467a96
Adnan Ahmed <adnan.ahmed01@gmail.com>
2020-01-18 16:26:15 +0000

    added oauth link

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { INSTAGRAM_APIKEY, REDIRECT_URL } from './config.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        { INSTAGRAM_APIKEY}
-        { REDIRECT_URL }
+      <header>
+        <p>Welcome to the Instagram Fetcher</p>
+        <p>{REDIRECT_URL}</p>
       </header>
+      <div>
+        <a href={`https://api.instagram.com/oauth/authorize
+        ?client_id=${INSTAGRAM_APIKEY}
+        &redirect_uri=${REDIRECT_URL}
+        &scope=user_profile,user_media
+        &response_type=code`} >
+          Sign In
+        </a>
+      </div>
     </div>
   );
 }
